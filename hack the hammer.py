e898eb4034e2225b5b8de87c13b4a18dcba787ef
F1NQB3R <34928592+F1NQB3R@users.noreply.github.com>
2018-02-04 01:09:39 -0500

    Add files via upload

---
+++ hack the hammer.py
@@ -0,0 +1,104 @@
+import sys
+import twitter
+import time
+import mraa
+
+buzz = mraa.Gpio(29)
+buzz.dir(mraa.DIR_OUT)
+
+touch = mraa.Gpio(31)
+touch.dir(mraa.DIR_IN)
+
+print("What time would you like your alarm? (24 hour format)")
+hour = int(input("Enter the hour: "))
+minute = int(input("Enter the minute: "))
+second = int(input("Enter the second: "))
+
+hour = hour - 19
+if hour < 0:
+    hour = 24 + hour
+
+bus = input("Are you a bus student? (Y/N) ")
+
+api = twitter.Api(consumer_key="EywEWq2n20NwBgGTyzIEeB0U8",
+                      consumer_secret="nHVSWuGpBCeQ4d33UHiRevjEqy5lgN8BeNFVNpZbaXF2TqrWGC",
+                      access_token_key="838169904499015681-9OQ5Xl6yVQK6IBhJKT62AYpYluxtUGd",
+                      access_token_secret="J5hiTjjKRZ4ATU3tF4r1LhO86Az8NZbgtcKtQGbd6QQJe")
+
+kevin = 838169904499015681
+aaron = 959804517494771712
+peel = 23796987
+
+statuses = api.GetUserTimeline(kevin)
+
+#non_bmp_map = dict.fromkeys(range(0x10000, sys.maxunicode + 1), 0xfffd)
+#print("Hi".translate(non_bmp_map))
+
+old = statuses[0].text
+
+alarm = True
+
+#main running loop
+while True:
+    buzz.write(0)
+    #print(alarm)
+    statuses = api.GetUserTimeline(kevin)
+    new = statuses[0].text
+    if new != old:
+        #print(old)
+        #print(new)
+        #print("check")
+        old = new
+        if bus == "Y":
+            if ("school" in new and ("close" in new or "cancel" in new)) or ("bus" in new and "cancel" in new):
+                alarm = False
+            else:
+                alarm = True
+        else:        
+            if "school" in new and ("close" in new or "cancel" in new):
+                alarm = False
+            else:
+                alarm = True
+
+    #print(time.localtime().tm_hour)
+
+    #alarm goes off at set time if alarm is on
+    #print(time.localtime().tm_hour, time.localtime().tm_min, time.localtime().tm_sec)
+    if time.localtime().tm_hour == hour  and time.localtime().tm_min == minute and time.localtime().tm_sec >= second:
+        #print("time reached")
+        while True:
+            if alarm:
+                #print("alarm goes off")
+                buzz.write(1)
+                tick = 0
+                press = False
+                snooze = False
+                while True:
+                    touchbutton = int(touch.read())
+                    if touchbutton == 1:
+                        tick+=1
+                        press = True
+
+                    elif tick>=30000:
+                        snooze = False
+                        break
+                    
+                    elif press and touchbutton == 0:
+                        snooze = True
+                        break
+                
+                alarm = False
+            else:
+                if snooze:
+                    print ("snooze")
+                    buzz.write(0)
+                    time.sleep(10)
+                    buzz.write(1)
+                    alarm = True
+                else:
+                    print("off")
+                    break
+
+    if time.localtime().tm_hour == 0  and time.localtime().tm_min == 0 and time.localtime().tm_sec == 0:
+        alarm = True
+
